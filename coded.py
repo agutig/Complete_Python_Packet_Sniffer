4dba2b96d3de3cfa5f2653ca4015daff828d2759
Álvaro Gutiérrez García <alvarogutierrezg99@gmail.com>
2022-10-01 13:46:47 +0200

    hhhhhhhh

--- coded.py
+++ coded.py
@@ -1,10 +1,26 @@
 #Source https://www.iana.org/assignments/protocol-numbers/protocol-numbers.xhtml
 
-eth_sub_protocols = {
+eth_sub_protocols = {  #TODO only works ipv4 --> Net layer
     0:"HOPOT",
     1:"ICMP",
     2:"IGMP",
     3:"GGP",
     8:"IPv4"
+
+}
+
+transport_sub_protocols = {  #Transport layer
+    1:"ICMP",
+    2:"IGMP",
+    6:"TCP",
+    9:"IGRP",
+    17:"UDP",
+    47:"GRE",
+    50:"ESP",
+    51:"AH",
+    57:"SKIP",
+    88:"EIGRP",
+    89:"OSPF",
+    115:"L2TP"
 
 }
--- sniffer.py
+++ sniffer.py
@@ -1,6 +1,6 @@
 """
-Note: This sniffer was made following Angel Perez's tutorial:
-https://www.uv.mx/personal/angelperez/files/2018/10/sniffers_texto.pdf
+Note: This sniffer was made following thenewboston's tutorial:
+https://www.youtube.com/watch?v=3zwuOo7U1YQ
 
 The main idea of this proyect is to reconvert it to my own version.
 """
@@ -10,13 +10,16 @@ from struct import *
 import socket,struct  #Libraries
 import coded #Program
 
-#parsing
+##parsing
 
+#LINK layer
 def get_mac(addr):
- return ':'.join(map('{:02x}'.format ,addr)).upper()
+    return ':'.join(map('{:02x}'.format ,addr)).upper()
+
+def get_ipv4(addr):
+    return '.'.join(map(str ,addr))
 
 def ethernet_head(raw_data): #MAC Adress
-
     dest, src, prototype = struct.unpack('! 6s 6s H', raw_data[:14])
     dest_mac = get_mac(dest)
     src_mac = get_mac(src)
@@ -24,16 +27,30 @@ def ethernet_head(raw_data): #MAC Adress
     data = raw_data[14:]
     return dest_mac, src_mac, proto, data
 
+def eth_print(eth):
+    print('\nEthernet Frame:')
+    print('MAC Destination: {}, MAC Source: {}, InnerProtocol: {}'.format(eth[0], eth[1], coded.eth_sub_protocols.get(eth[2] ,eth[2] )))
 
+
+#NET layer
 def ipv4_head(raw_data):
     version_header_length = raw_data[0]
     version = version_header_length >> 4
     header_length = (version_header_length & 15) * 4
     ttl, proto, src, target = struct.unpack('! 8x B B 2x 4s 4s', raw_data[:20])
     data = raw_data[header_length:]
-    return version, header_length, ttl, proto, src, target, data
+    return version, header_length, ttl, proto, get_ipv4(src), get_ipv4(target), data[header_length:]
 
 
+def ipv4_print(ipv4):
+    print( '\t - ' + 'IPv4 Packet:')
+    print('\t\t - ' + 'Version: {}, Header Length: {}, TTL:{},'.format(ipv4[0], ipv4[1], ipv4[2]))
+    print('\t\t - ' + 'Protocol: {}, Source: {}, Target:{}'.format(coded.transport_sub_protocols.get(ipv4[3] ,ipv4[3] ), ipv4[4], ipv4[5]))
+
+    return ipv4[3] ,ipv4[6] #Inner protocol + inner protocol data
+
+
+#Transport Layer
 def tcp_head( raw_data):
     (src_port, dest_port, sequence, acknowledgment, offset_reserved_flags) = struct.unpack( '! H H L L H', raw_data[:14])
     offset = (offset_reserved_flags >> 12) * 4
@@ -43,8 +60,7 @@ def tcp_head( raw_data):
     flag_rst = (offset_reserved_flags & 4) >> 2
     flag_syn = (offset_reserved_flags & 2) >> 1
     flag_fin = offset_reserved_flags & 1
-    data = raw_data[offset:]
-    return src_port, dest_port, sequence, acknowledgment, flag_urg, flag_ack, flag_psh, flag_rst, flag_syn, flag_fin, data
+    return src_port, dest_port, sequence, acknowledgment, flag_urg, flag_ack, flag_psh, flag_rst, flag_syn, flag_fin, raw_data[offset:]
 
 #Main loop
 def main():
@@ -54,36 +70,18 @@ def main():
         print("-------------------------------------------")
         raw_data, addr = s.recvfrom(65535)
         eth = ethernet_head(raw_data)
-        print('\nEthernet Frame:')
-        print('MAC Destination: {}, MAC Source: {}, InnerProtocol: {}'.format(eth[0], eth[1], coded.eth_sub_protocols.get(eth[2] ,eth[2] )))
+        eth_print(eth)
         if eth[2] == 8:
             ipv4 = ipv4_head(eth[3])
-            print( '\t - ' + 'IPv4 Packet:')
-            print('\t\t - ' + 'Version: {}, Header Length: {}, TTL:{},'.format(ipv4[1], ipv4[2], ipv4[3]))
-            print('\t\t - ' + 'Protocol: {}, Source: {}, Target:{}'.format(ipv4[4], ipv4[5], ipv4[6]))
-            if ipv4[4] == 6:
-                tcp = tcp_head(ipv4[7])
-                print(TAB_1 + 'TCP Segment:')
-                print(TAB_2 + 'Source Port: {}, Destination Port: {}'.format(tcp[0], tcp[1]))
-                print(TAB_2 + 'Sequence: {}, Acknowledgment: {}'.format(tcp[2], tcp[3]))
-                print(TAB_2 + 'Flags:')
-                print(TAB_3 + 'URG: {}, ACK: {}, PSH:{}'.format(tcp[4], tcp[5], tcp[6]))
-                print(TAB_3 + 'RST: {}, SYN: {}, FIN:{}'.format(tcp[7], tcp[8], tcp[9]))
-                if len(tcp[10]) > 0:
-                # HTTP
-                    if tcp[0] == 80 or tcp[1] == 80:
-                        print(TAB_2 + 'HTTP Data:')
-                        try:
-                            http = HTTP(tcp[10])
-                            http_info = str(http[10]).split('\n')
-                            for line in http_info:
-                                print(DATA_TAB_3 + str(line))
-                        except:
-                            print(format_multi_line(DATA_TAB_3, tcp[10]))
-                    else:
-                        print(TAB_2 + 'TCP Data:')
-                        print(format_multi_line(DATA_TAB_3, tcp[10]))
-
+            trans_protocol ,trans_data = ipv4_print(ipv4)
+            if trans_protocol == 6: #TCP 
+                tcp = tcp_head(trans_data)
+                print('TCP Segment:')
+                print('Source Port: {}, Destination Port: {}'.format(tcp[0], tcp[1]))
+                print('Sequence: {}, Acknowledgment: {}'.format(tcp[2], tcp[3]))
+                print('Flags:')
+                print('URG: {}, ACK: {}, PSH:{}'.format(tcp[4], tcp[5], tcp[6]))
+                print('RST: {}, SYN: {}, FIN:{}'.format(tcp[7], tcp[8], tcp[9]))
 main()
 
 
