e38f0af4b4ba2685a05e90f845f445346484b5ff
Álvaro Gutiérrez García <alvarogutierrezg99@gmail.com>
2022-10-05 14:50:01 +0200

    aa

--- UI.py
+++ UI.py
@@ -4,20 +4,39 @@ import sys
 
 def main():
 
-    encrypted = False
-    sys.argv[1] = ""
+    hide = True
+    try:
 
-    if (sys.argv[1] == "c" ):
+        hide = True if  (sys.argv[1] == "c") else False
 
+        iface_number = 0
         for i in net_if_addrs().items():
             print("")
-            print('\t\t Iface:'+ str(i[0]))
+            print('\t\t' + str(iface_number) + '.  Iface: '+ str(i[0]))
             for j in i[1:]:
                 for h in j:
-                    print('\t\t  - ' + 'family: ' + str(h.family).split(".")[1] + ' | address: ' + str(h.address) + ' | netmask: ' + str(h.netmask) + ' | broadcast: ' + str(h.broadcast)  + ' | ptp: ' + str(h.ptp))        
-        print("Sniffer Listening -> Local Machine:")
+                    address = ""
+                    
+                    if (not hide):
+                        address = str(h.address)
+                    else:
+                        if str(h.family).split(".")[1]  == "AF_INET":
+                            address = "LOCAL IP"
+                        elif str(h.family).split(".")[1]  == "AF_INET6":
+                            address = "LOCAL MAC"
+                        else:
+                            address = "LOCAL ADDRESS"
 
-    else:
-        print("please init with a mode: c for encrypted addresses , p for visualizating adresses ")
+                    print('\t\t\t  - ' + 'family: ' + str(h.family).split(".")[1] + 
+                    ' | address: ' +  (str(address)) 
+                    + ' | netmask: ' + str(h.netmask) + ' | broadcast: ' + str(h.broadcast)  + ' | ptp: ' + str(h.ptp))        
+            iface_number += 1    
+        print("")
+        print('\t\t' + str(iface_number) + '. ALL ifaces')
+        print("")
+        print("Sniffer Listening -> Local Machine:")  
+    
+    except  IndexError:
+        print("please init with a mode: c to hide local addresses , p for visualizating all adresses ")
 
 main()
